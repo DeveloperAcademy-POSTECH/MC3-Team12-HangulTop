f57f41fba726b68626e721476d2562500648e743
JIMIN KIM <jim4020key@gmail.com>
2022-07-18 02:53:58 +0900

    Feat: ConsonantViewController 파일 생성 (#6)

---
+++ HangulTop/ViewController/ConsonantViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ConsonantViewController.swift
+//  HangulTop
+//
+//  Created by kimjimin on 2022/07/18.
+//
+
+import UIKit
+
+class ConsonantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationItem.hidesBackButton = true
+    }
+}
