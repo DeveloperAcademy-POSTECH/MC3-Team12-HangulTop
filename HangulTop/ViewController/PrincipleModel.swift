4a4d5b3db1194d78e9896d5b575fbb9355451971
Seulki Lee <operry126@gmail.com>
2022-07-18 17:37:53 +0900

    Feat. applied MVC pattern

---
+++ HangulTop/ViewController/PrincipleModel.swift
@@ -0,0 +1,71 @@
+//
+//  PrincipleModel.swift
+//  HangulTop
+//
+//  Created by Seulki Lee on 2022/07/18.
+//
+
+import Foundation
+import UIKit
+
+// 창제원리 뷰 로직이 담긴 구조체
+struct PrincipleModel {
+    
+    // 화면에 나올 컨텐츠 데이터
+    let contents = [
+        PrincipleTexts(title: "한글은 누가 만들었을까?", subtitle: "", principleImage: nil, letters: [], info: "한글은 한국어의 공식문자이다.\n1443년 조선의 왕 세종이 한국어를 표기하기 위해 ‘훈민정음'이란 문자를 창제했고, 20세기 초반부터는 한글이란 명칭으로 부른다."),
+        PrincipleTexts(title: "한글은 누가 만들었을까?", subtitle: "", principleImage: nil, letters: [], info: "세종대왕은 한자(표의문자)를 몰라 어려움을 겪는 백성들은 안타깝게 여겨 누구나 배우기 쉬운 글자(표음문자)을 만들었다."),
+        PrincipleTexts(title: "한글은 누가 만들었을까?", subtitle: "", principleImage: nil, letters: [], info: "한글은 자음 14개와 모음 10개, 이렇게 글자 24개만 익히면 낱자와 소리를 연결해 글을 읽을 수 있다."),
+        PrincipleTexts(title: "자음은 어떻게 만들어졌을까?", subtitle: "", principleImage: nil, letters: [], info: "세종대왕은 이,혀 입술 목 등 사람이 발음하는 기관의 모양을 본떠서 자음 글자를 만들었다.\n이렇게 다섯개의 자음 글자를 만들고, 이것을 바탕으로 나머지 자음 글자를 완성하셨다."),
+        PrincipleTexts(title: "자음은 어떻게 만들어졌을까?", subtitle: "", principleImage: nil, letters: [], info: "다섯개의 기본 글자에 획을 더하여 강한 소리를 내는 자음들이 만들어졌다."),
+        PrincipleTexts(title: "어금닛소리", subtitle: "혓등이 여린 입천장에 닿는 모양", principleImage: nil, letters: ["ㄱ", "ㅋ", "ㄲ"], info: ""),
+        PrincipleTexts(title: "혓소리", subtitle: "혀끝이 윗니 바로 뒤의 잇몸에 닿는 모양", principleImage: nil, letters: ["ㄴ", "ㄷ", "ㅌ", "ㄹ", "ㄸ"], info: ""),
+        PrincipleTexts(title: "입술소리", subtitle: "아랫입술과 윗입술이 다물어진 모양", principleImage: nil, letters: ["ㅁ", "ㅂ", "ㅍ", "ㅃ"], info: ""),
+        PrincipleTexts(title: "잇소리", subtitle: "이의 모양", principleImage: nil, letters: ["ㅅ", "ㅈ", "ㅊ", "ㅉ", "ㅆ"], info: ""),
+        PrincipleTexts(title: "목구멍소리", subtitle: "목구멍의 모양", principleImage: nil, letters: ["ㅇ", "ㅎ"], info: ""),
+        PrincipleTexts(title: "모음은 어떻게 만들어졌을까?", subtitle: "", principleImage: nil, letters: [], info: "세종대왕은 (동양 철학에서) 만물의 근본 요소로 생각하는 세 가지를 보고 모음 글자를 만들었다.\n하늘의 모습을 본떠 .\n땅의 모습을 본떠 ㅡ\n사람의 모습을 본떠 ㅣ\n이것을 ‘천지인’이라고 부른다."),
+        PrincipleTexts(title: "모음은 어떻게 만들어졌을까?", subtitle: "", principleImage: nil, letters: ["ㅡ", "∙", "ㅣ"], info: "이 모음들 중 ∙(아래 아)는 현재 단독으로 쓰이지 않는데, 그것은 이 글자가 나타내는 소리가 지금은 없어졌기 때문이다."),
+        PrincipleTexts(title: "모음은 어떻게 만들어졌을까?", subtitle: "", principleImage: nil, letters: ["ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅗ", "ㅛ", "ㅜ", "ㅠ", "+"], info: "나머지 모음글자는 어떻게 만들어졌을까?\nㅣ와 ㅡ에 .을 붙이는 방법으로 만들었다"),
+        PrincipleTexts(title: "", subtitle: "", principleImage: nil, letters: ["ㅎ", "ㅏ", "ㄴ", "ㄱ", "ㅡ", "ㄹ"], info: "자음과 모음은 따로 떨어져 있어선 안된다\n함께 붙어있어야만 뜻이 있는 소리가 된다")
+    ]
+    
+    // 몇 번째 뷰인지 카운트
+    var pageCount = 0
+
+    // 큰 제목 리턴하는 함수
+    func getTitle() -> String {
+        contents[pageCount].title
+    }
+    
+    // 작은 제목 리턴하는 함수
+    func getSubtitle() -> String {
+        contents[pageCount].subtitle
+    }
+    
+    // 이미지(옵셔널) 리턴하는 함수
+    func getImage() -> UIImageView? {
+        contents[pageCount].principleImage
+    }
+    
+    // 컬렉션뷰에 들어갈 문자들의 배열 리턴하는 함수
+    func getLetters() -> [String] {
+        contents[pageCount].letters
+    }
+    
+    // 설명 부분 리턴하는 함수
+    func getInfo() -> String {
+        contents[pageCount].info
+    }
+    
+    // 다음 뷰로 넘어갈 때 페이지 카운트 + 1 해주는 함수
+    mutating func nextView() {
+        if pageCount < contents.count - 1 {
+            self.pageCount += 1
+        }
+    }
+    
+    // 이전 뷰로 돌아갈 때 페이지 카운트 - 1 해주는 함수
+    mutating func prevView() {
+        self.pageCount -= 1
+    }
+}
--- HangulTop/ViewController/PrincipleViewController.swift
+++ HangulTop/ViewController/PrincipleViewController.swift
@@ -11,30 +11,54 @@ class PrincipleViewController: UIViewController {
     @IBOutlet weak var progressNum: UILabel!
     @IBOutlet weak var principleTitle: UILabel!
     @IBOutlet weak var principleSubtitle: UILabel!
-    @IBOutlet weak var principleImage: UIImageView!
+    @IBOutlet weak var principleImage: UIImageView?
     @IBOutlet weak var explanation: UILabel!
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     // 현재 버튼들 cornerRadius 전부 key path로 직접 숫자 집어넣었는데 이거 비율로 바꾸고 싶음
     
+    var principleModel = PrincipleModel() //초기화, 인스턴스
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Principles"
-        progressNum.text = "1/14"
-        principleTitle.text = "한글은 누가 만들었을까?"
-        explanation.text = "한글은 한국어의 공식문자이다.\n1443년 조선의 왕 세종이 한국어를 표기하기 위해 ‘훈민정음'이란 문자를 창제했고, 20세기 초반부터는 한글이란 명칭으로 부른다."
-        prevButton.isHidden = true
-        nextButton.isHidden = true
-        principleSubtitle.isHidden = true
+        updateUI()
     }
     
+    // 첫 번째 next 버튼 눌렀을 때 뷰 업데이트 해주는 함수. 근데 동적으로 사이즈 조절 가능하다면 nextButton이랑 통합해서 중복 줄여줄 수 있을 것 같음
     @IBAction func firstButtonPressed(_ sender: UIButton) {
+        principleModel.nextView() // 두 번째 뷰로 넘어가기 위해 카운트 + 1
+        updateUI()
     }
     
+    // 이전 버튼 눌렀을 때 뷰 업데이트 해주는 함수
     @IBAction func prevButtonPressed(_ sender: UIButton) {
+        principleModel.prevView() // 이전 뷰로 돌아가기 위해 카운트 - 1
+        updateUI()
     }
     
+    // 다음 버튼 눌렀을 때 뷰 업데이트 해주는 함수
     @IBAction func nextButtonPressed(_ sender: UIButton) {
+        principleModel.nextView() // 다음 뷰로 넘어가기 위해 카운트 + 1
+        updateUI()
+    }
+    
+    // UI 업데이트 해주는 함수
+    func updateUI() {
+        progressNum.text = "\(principleModel.pageCount + 1)/14"
+        principleTitle.text = principleModel.getTitle()
+        principleSubtitle.text = principleModel.getSubtitle()
+        principleImage = principleModel.getImage()
+        explanation.text = principleModel.getInfo()
+        if principleModel.pageCount == 0 { // 첫 번째 뷰에서는 큰 next 버튼만 보이게 하고 작은 prev, next버튼은 숨김
+            firstButton.isHidden = false
+            prevButton.isHidden = true
+            nextButton.isHidden = true
+        } else { // 이외의 뷰에서는 큰 next 버튼 숨기고 작은 prev, next버튼은 보이게
+            firstButton.isHidden = true
+            nextButton.isHidden = false
+            prevButton.isHidden = false
+        }
     }
 }
