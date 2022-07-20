2181788cafc3b7b7eec3760a0b5d3e21a737f5b9
Green <76623853+jim4020key@users.noreply.github.com>
2022-07-20 21:37:37 +0900

    :twisted_rightwards_arrows: Feat: Implement Views in Batchim (#19)
    * Chore: Delete unused file
    
    * Feat: Add views in Batchim.storyboard
    
    * Feat: Add Buttonset View
    
    * Feat: Add Start View
    
    * Feat: Add Finish Page
    
    * Feat: Add PageIndex View
    
    Co-authored-by: Juhwa Lee <juhwa.lee1023@gmail.com>

--- HangulTop/ViewController/BatchimModel.swift
+++ HangulTop/ViewController/BatchimModel.swift
@@ -9,52 +9,48 @@ import Foundation
 import UIKit
 
 struct BatchimTexts {
-    //+이걸 다른 struct에 쓰는 이유는? /이렇게 한 문서에 같이 써도 되나?
-        let letters: [String]
-        let info: String
+    let letters: [String]
+    let info: String
 }
 
 struct BatchimModel {
     
     let contents = [
-        BatchimTexts(letters: ["ㄱ", "ㅋ", "ㄲ"], info: "1악"),
-        BatchimTexts(letters: ["ㄴ", "ㄵ", "ㄶ"], info: "2안"),
-        BatchimTexts(letters: ["ㄷ", "ㅅ", "ㅆ", "ㅈ", "ㅊ", "ㅌ", "ㅎ"], info: "3앋"),
-        BatchimTexts(letters: ["ㄹ", "ㄼ", "ㄽ", "ㄾ", "ㅀ"], info: "4알"),
-        BatchimTexts(letters: ["ㅁ", "ㄻ", "ㄲ"], info: "5앎"),
-        BatchimTexts(letters: ["ㅂ", "ㅍ", "ㅄ", "ㄿ"], info: "6압"),
-        BatchimTexts(letters: ["ㅇ"], info: "7앙"),
-    ]    
-    
+        BatchimTexts(letters: ["ㄱ", "ㅋ", "ㄲ"], info: ""),
+        BatchimTexts(letters: ["ㄴ", "ㄵ", "ㄶ"], info: ""),
+        BatchimTexts(letters: ["ㄷ", "ㅅ", "ㅆ", "ㅈ", "ㅊ", "ㅌ", "ㅎ"], info: "ㄸ, ㅉ은 받침으로 쓰지 않는다."),
+        BatchimTexts(letters: ["ㄹ", "ㄼ", "ㄽ", "ㄾ", "ㅀ"], info: ""),
+        BatchimTexts(letters: ["ㅁ", "ㄻ", "ㄲ"], info: ""),
+        BatchimTexts(letters: ["ㅂ", "ㅍ", "ㅄ", "ㄿ"], info: "ㅃ은 받침으로 쓰지 않는다."),
+        BatchimTexts(letters: ["ㅇ"], info: "ㅇ은 첫소리에서 소릿값이 없고, 받침으로 올때만 소리를 인식할 수 있다."),
+    ]
     
     var pageCount = 0
-
-
-    // 컬렉션뷰에 들어갈 문자들의 배열 리턴하는 함수
+    
+    func getBtnCount() -> Int {
+        contents[pageCount].letters.count
+    }
+    
     func getLetters() -> [String] {
         contents[pageCount].letters
     }
-
-    //여기서 if []안에 있는게 n개면 -> 버튼을 이렇게 배치해라
-    // 1, 3, 5
-    // 2, 4, 6, 7
-    
-    
     
     // 설명 부분 리턴하는 함수
     func getInfo() -> String {
         contents[pageCount].info
     }
-
+    
     // 다음 뷰로 넘어갈 때 페이지 카운트 + 1 해주는 함수
     mutating func nextView() {
-        if pageCount < contents.count - 1 {
+        if pageCount <= contents.count - 1 {
             self.pageCount += 1
         }
     }
-
+    
     // 이전 뷰로 돌아갈 때 페이지 카운트 - 1 해주는 함수
     mutating func prevView() {
-        self.pageCount -= 1
+        if pageCount > 0 {
+            self.pageCount -= 1
+        }
     }
 }
