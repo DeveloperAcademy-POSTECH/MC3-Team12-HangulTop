5a6c953a2e68ae0ae705d33dbc74907fc8f98580
100seo <deepgreen7362@gmail.com>
2022-07-20 18:07:23 +0900

    :twisted_rightwards_arrows: Feat: LastVowelView (#16)
    * Feat: Add Vowel View (#4)
    
    모음 뷰 구현
    
    * feat: vowel page view (#4)
    
    * 첫번째 시작 설명 뷰와 마지막 페이지 뷰 작성

---
+++ HangulTop/ViewController/LastVowelViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LastVowelViewController.swift
+//  HangulTop
+//
+//  Created by baek seohyeon on 2022/07/20.
+//
+
+import UIKit
+
+class LastVowelViewController: UIViewController {
+
+    
+    @IBOutlet weak var finishExplanation: UILabel!
+    
+    var caption: String = "모든 학습을 마쳤습니다."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        finishExplanation.text = caption
+    }
+
+}
