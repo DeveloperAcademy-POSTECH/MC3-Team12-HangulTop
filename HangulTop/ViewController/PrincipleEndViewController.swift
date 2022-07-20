44a576e02d897075777b8cbb704dd5e584cf1974
Seulki Lee <operry126@gmail.com>
2022-07-20 15:35:08 +0900

    feat backtomain button

--- HangulTop/ViewController/PrincipleEndViewController.swift
+++ HangulTop/ViewController/PrincipleEndViewController.swift
@@ -20,16 +20,10 @@ class PrincipleEndViewController: UIViewController {
     // 메뉴로 돌아가는 코드
     @IBAction func menuButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: false)
+        self.navigationController?.popToRootViewController(animated: true)
         
-        /* 메뉴로 돌아가는 코드, 다음 메뉴 열리는 코드 들어가야 함
-         
-         If you want to go back to the previous view controller
-         _ = navigationController?.popViewController(animated: true)
-         
-         If you want to go back to the root view controller
-         _ = navigationController?.popToRootViewController(animated: true)
-         
-         이 두 경우 중 메뉴뷰에서 원리뷰로 넘어오는 코드가 어떻게 구현된지 몰라서 + 아직 머지되지 않았기 때문에 우선은 생략하고 넘어감
-         */
+
+//        메뉴로 돌아가는 코드, 다음 메뉴 열리는 코드 들어가야 함
+//        _ = navigationController?.popToViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
     }
 }
