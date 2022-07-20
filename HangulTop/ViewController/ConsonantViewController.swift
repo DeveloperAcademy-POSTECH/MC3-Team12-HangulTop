7f9ed689b98b5dbc74f53baa206180cbbeb2b9e6
JIMIN KIM <jim4020key@gmail.com>
2022-07-20 17:14:51 +0900

    Temp: Change ConsonantVC for applying actions

--- HangulTop/ViewController/ConsonantViewController.swift
+++ HangulTop/ViewController/ConsonantViewController.swift
@@ -13,6 +13,18 @@ class ConsonantViewController: UIViewController, UICollectionViewDataSource,UICo
     let consonantArray = [["ㄱ","ㅋ","ㄲ"], ["ㄴ","ㄷ","ㅌ","ㄹ","ㄸ"], ["ㅁ", "ㅂ","ㅍ","ㅃ"], ["ㅅ","ㅈ","ㅊ","ㅉ","ㅆ"], ["ㅇ","ㅎ"]]
     let vowelArray = ["ㅡ", "ㅣ", "ㅏ", "ㅓ", "ㅗ", "ㅜ", "ㅑ", "ㅕ", "ㅛ", "ㅠ", "ㅐ", "ㅔ", "ㅒ", "ㅖ", "ㅘ", "ㅚ", "ㅙ", "ㅝ", "ㅟ", "ㅞ", "ㅢ"]
     
+    @IBOutlet weak var mainLetter: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBAction func buttonSelected(_ sender: UIButton) {
+        mainLetter.text = sender.titleLabel?.text ?? "ㄱ"
+    }
+    
     @IBOutlet weak var page1: UILabel!
     @IBOutlet weak var page2: UILabel!
     @IBOutlet weak var page3: UILabel!
@@ -26,7 +38,7 @@ class ConsonantViewController: UIViewController, UICollectionViewDataSource,UICo
     @IBAction func prevPage(_ sender: Any) {
         if pageNum > 0 {
             pageNum -= 1
-            setLayout(ConsonantViewController(), customView, btnList: consonantArray[pageNum])
+            setButtonLayout()
             setPageControl()
         }
     }
@@ -36,7 +48,7 @@ class ConsonantViewController: UIViewController, UICollectionViewDataSource,UICo
             if pageNum == consonantArray.count {
                 performSegue(withIdentifier: "finish_seg", sender: sender)
             } else {
-                setLayout(ConsonantViewController(), customView, btnList: consonantArray[pageNum])
+                setButtonLayout()
                 setPageControl()
             }
         }
@@ -58,12 +70,12 @@ class ConsonantViewController: UIViewController, UICollectionViewDataSource,UICo
     }
     
     @objc func getVowel(sender: UIButton){
-        print(sender.titleLabel?.text ?? "error")
+        mainLetter.text = sender.titleLabel?.text ?? "ㅏ"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout(ConsonantViewController(), customView, btnList: consonantArray[pageNum])
+        setButtonLayout()
         pageArray = [page1, page2, page3, page4, page5]
         setPageControl()
     }
@@ -79,61 +91,59 @@ class ConsonantViewController: UIViewController, UICollectionViewDataSource,UICo
         }
         pageArray[pageNum].textColor = .black
     }
-}
-
-func setLayout(_ VC: UIViewController, _ customView: UIView, btnList: [String]) {
-    let btnCnt = btnList.count
-    let nibs = Bundle.main.loadNibNamed("ButtonSetView", owner: VC)
-    guard let buttonSetView = nibs?.first as? ButtonSetView else {
-        return
-    }
-    buttonSetView.frame = customView.bounds
-    customView.addSubview(buttonSetView)
     
-    if btnCnt == 1 {
-        buttonSetView.button1.isHidden = true
-        buttonSetView.button2.isHidden = true
-        buttonSetView.button3.isHidden = true
-        buttonSetView.button4.isHidden = true
-        buttonSetView.button5.isHidden = true
-        buttonSetView.button6.setTitle(btnList[0], for: .normal)
-        buttonSetView.button7.isHidden = true
-    }
-    if btnCnt == 2 {
-        buttonSetView.button1.isHidden = true
-        buttonSetView.button2.setTitle(btnList[0], for: .normal)
-        buttonSetView.button3.setTitle(btnList[1], for: .normal)
-        buttonSetView.button4.isHidden = true
-        buttonSetView.button5.isHidden = true
-        buttonSetView.button6.isHidden = true
-        buttonSetView.button7.isHidden = true
-    }
-    if btnCnt == 3 {
-        buttonSetView.button1.isHidden = true
-        buttonSetView.button2.isHidden = true
-        buttonSetView.button3.isHidden = true
-        buttonSetView.button4.isHidden = true
-        buttonSetView.button5.setTitle(btnList[0], for: .normal)
-        buttonSetView.button6.setTitle(btnList[1], for: .normal)
-        buttonSetView.button7.setTitle(btnList[2], for: .normal)
-    }
-    if btnCnt == 4 {
-        buttonSetView.button1.setTitle(btnList[0], for: .normal)
-        buttonSetView.button2.setTitle(btnList[1], for: .normal)
-        buttonSetView.button3.setTitle(btnList[2], for: .normal)
-        buttonSetView.button4.setTitle(btnList[3], for: .normal)
-        buttonSetView.button5.isHidden = true
-        buttonSetView.button6.isHidden = true
-        buttonSetView.button7.isHidden = true
-    }
-    if btnCnt == 5 {
-        buttonSetView.button1.isHidden = true
-        buttonSetView.button2.setTitle(btnList[0], for: .normal)
-        buttonSetView.button3.setTitle(btnList[1], for: .normal)
-        buttonSetView.button4.isHidden = true
-        buttonSetView.button5.setTitle(btnList[2], for: .normal)
-        buttonSetView.button6.setTitle(btnList[3], for: .normal)
-        buttonSetView.button7.setTitle(btnList[4], for: .normal)
+    func setButtonLayout() {
+        if consonantArray[pageNum].count == 2 {
+            button1.isHidden = true
+            button2.isHidden = false
+            button3.isHidden = false
+            self.button2.setTitle(consonantArray[pageNum][0], for: .normal)
+            self.button3.setTitle(consonantArray[pageNum][1], for: .normal)
+            button4.isHidden = true
+            button5.isHidden = true
+            button6.isHidden = true
+            button7.isHidden = true
+        }
+        if consonantArray[pageNum].count == 3 {
+            button1.isHidden = true
+            button2.isHidden = true
+            button3.isHidden = true
+            button4.isHidden = true
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = false
+            self.button5.setTitle(consonantArray[pageNum][0], for: .normal)
+            self.button6.setTitle(consonantArray[pageNum][1], for: .normal)
+            self.button7.setTitle(consonantArray[pageNum][2], for: .normal)
+        }
+        if consonantArray[pageNum].count == 4 {
+            button1.isHidden = false
+            button2.isHidden = false
+            button3.isHidden = false
+            button4.isHidden = false
+            self.button1.setTitle(consonantArray[pageNum][0], for: .normal)
+            self.button2.setTitle(consonantArray[pageNum][1], for: .normal)
+            self.button3.setTitle(consonantArray[pageNum][2], for: .normal)
+            self.button4.setTitle(consonantArray[pageNum][3], for: .normal)
+            button5.isHidden = true
+            button6.isHidden = true
+            button7.isHidden = true
+        }
+        if consonantArray[pageNum].count == 5 {
+            button1.isHidden = true
+            button2.isHidden = false
+            button3.isHidden = false
+            self.button2.setTitle(consonantArray[pageNum][0], for: .normal)
+            self.button3.setTitle(consonantArray[pageNum][1], for: .normal)
+            button4.isHidden = true
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = false
+            self.button5.setTitle(consonantArray[pageNum][2], for: .normal)
+            self.button6.setTitle(consonantArray[pageNum][3], for: .normal)
+            self.button7.setTitle(consonantArray[pageNum][4], for: .normal)
+        }
+        
     }
 }
 
