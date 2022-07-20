2578a5910a04b39265436399fae94b91cb2b297f
JIMIN KIM <jim4020key@gmail.com>
2022-07-20 17:58:29 +0900

    Feat: Add Buttonset View

--- HangulTop/ViewController/BatchimViewController.swift
+++ HangulTop/ViewController/BatchimViewController.swift
@@ -11,63 +11,117 @@ class BatchimViewController: UIViewController {
     
     @IBOutlet weak var explanation: UILabel!
     @IBOutlet weak var mainLetter: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBAction func buttonSelected(_ sender: UIButton) {
+        mainLetter.text = sender.titleLabel?.text ?? "error"
+    }
     
-    
-    var batchimModel = BatchimModel() //초기화, 인스턴스?
-    
-    
+    var batchimModel = BatchimModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-    
-    
-    
-//    @IBAction func didTapButton1(_ sender: UIButton) {
-//        resultLabel.text = button1.title(for: .normal)
-//    }
-//    버튼을 눌렀을 때 resultLabel이 바뀌었으면 하는데 아무일도 일어나지 않음
-    
-
-    
 
     //다음 +1
     @IBAction func didTapNextButton(_ sender: UIButton) {
         batchimModel.nextView()
         updateUI()
     }
-    
-    
+
     //이전 -1
     @IBAction func didTapPrevButton(_ sender: UIButton) {
         batchimModel.prevView()
         updateUI()
     }
     
-    
-    //설명 업데이트
     func updateUI() {
-//        button1.title(for: .normal) = batchimModel.getLetters()
-//        ㄴ버튼 타이틀이 업데이트 할 때 마다 바뀌었으면 좋겠다
+        if batchimModel.getBtnCount() == 1 {
+            button1.isHidden = true
+            button2.isHidden = false
+            button3.isHidden = true
+            button4.isHidden = true
+            button5.isHidden = true
+            button6.isHidden = true
+            button7.isHidden = true
+            button2.setTitle(batchimModel.getLetters()[0], for: .normal)
+        }
+        if batchimModel.getBtnCount() == 2 {
+            button1.isHidden = true
+            button2.isHidden = true
+            button3.isHidden = true
+            button4.isHidden = true
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = true
+            button5.setTitle(batchimModel.getLetters()[0], for: .normal)
+            button6.setTitle(batchimModel.getLetters()[1], for: .normal)
+        }
+        if batchimModel.getBtnCount() == 3 {
+            button1.isHidden = false
+            button2.isHidden = false
+            button3.isHidden = false
+            button4.isHidden = true
+            button5.isHidden = true
+            button6.isHidden = true
+            button7.isHidden = true
+            button1.setTitle(batchimModel.getLetters()[0], for: .normal)
+            button2.setTitle(batchimModel.getLetters()[1], for: .normal)
+            button3.setTitle(batchimModel.getLetters()[2], for: .normal)
+        }
+        if batchimModel.getBtnCount() == 4 {
+            button1.isHidden = true
+            button2.isHidden = true
+            button3.isHidden = true
+            button4.isHidden = false
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = false
+            button4.setTitle(batchimModel.getLetters()[0], for: .normal)
+            button5.setTitle(batchimModel.getLetters()[1], for: .normal)
+            button6.setTitle(batchimModel.getLetters()[2], for: .normal)
+            button7.setTitle(batchimModel.getLetters()[3], for: .normal)
+        }
+        if batchimModel.getBtnCount() == 5 {
+            button1.isHidden = false
+            button2.isHidden = false
+            button3.isHidden = true
+            button4.isHidden = false
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = true
+            button1.setTitle(batchimModel.getLetters()[0], for: .normal)
+            button2.setTitle(batchimModel.getLetters()[1], for: .normal)
+            button4.setTitle(batchimModel.getLetters()[2], for: .normal)
+            button5.setTitle(batchimModel.getLetters()[3], for: .normal)
+            button6.setTitle(batchimModel.getLetters()[4], for: .normal)
+        }
+        if batchimModel.getBtnCount() == 7 {
+            button1.isHidden = false
+            button2.isHidden = false
+            button3.isHidden = false
+            button4.isHidden = false
+            button5.isHidden = false
+            button6.isHidden = false
+            button7.isHidden = false
+            button1.setTitle(batchimModel.getLetters()[0], for: .normal)
+            button2.setTitle(batchimModel.getLetters()[1], for: .normal)
+            button3.setTitle(batchimModel.getLetters()[2], for: .normal)
+            button4.setTitle(batchimModel.getLetters()[3], for: .normal)
+            button5.setTitle(batchimModel.getLetters()[4], for: .normal)
+            button6.setTitle(batchimModel.getLetters()[5], for: .normal)
+            button7.setTitle(batchimModel.getLetters()[6], for: .normal)
+        }
         explanation.text = batchimModel.getInfo()
     }
     
-    
-    
-    
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setButtonLayout() {
+        
     }
-    */
-
 }
