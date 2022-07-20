ec99a318ecbff553cd52532c37d743eb38315d11
Seulki Lee <89380341+eeunho@users.noreply.github.com>
2022-07-20 23:15:53 +0900

    :twisted_rightwards_arrows: Feat/invention principle (#24)
    * Feat combined func for action button pressed
    
    * Feat added end view and view controller
    
    * feat updated buttonpressed function
    
    * refactor edited code
    
    * feat backtomain button
    
    * style added letter buttons
    
    * style added hangul label
    
    Co-authored-by: Juhwa Lee <juhwa.lee1023@gmail.com>

---
+++ HangulTop/ViewController/PrincipleEndViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrincipleEndViewController.swift
+//  HangulTop
+//
+//  Created by Seulki Lee on 2022/07/19.
+//
+
+import UIKit
+
+class PrincipleEndViewController: UIViewController {
+
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        message.text = "Congratulations! You have finished the Principle of the Invention of Hangul." // 임시 메시지
+    }
+    
+    // 메뉴로 돌아가는 코드
+    @IBAction func menuButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: false)
+        self.navigationController?.popToRootViewController(animated: true)
+        
+
+//        메뉴로 돌아가는 코드, 다음 메뉴 열리는 코드 들어가야 함
+//        _ = navigationController?.popToViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+    }
+}
--- HangulTop/ViewController/PrincipleViewController.swift
+++ HangulTop/ViewController/PrincipleViewController.swift
@@ -17,6 +17,14 @@ class PrincipleViewController: UIViewController {
     @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     // 현재 버튼들 cornerRadius 전부 key path로 직접 숫자 집어넣었는데 이거 비율로 바꾸고 싶음
+    @IBOutlet weak var mConsonants: UIView!
+    @IBOutlet weak var gConsonants: UIView!
+    @IBOutlet weak var sConsonants: UIView!
+    @IBOutlet weak var nConsonants: UIView!
+    @IBOutlet weak var hConsonants: UIView!
+    @IBOutlet weak var basicVowels: UIView!
+    @IBOutlet weak var compoundVowels: UIView!
+    @IBOutlet weak var hangulLabel: UIView!
     
     var principleModel = PrincipleModel() //초기화, 인스턴스
     
@@ -26,22 +34,20 @@ class PrincipleViewController: UIViewController {
         updateUI()
     }
     
-    // 첫 번째 next 버튼 눌렀을 때 뷰 업데이트 해주는 함수. 근데 동적으로 사이즈 조절 가능하다면 nextButton이랑 통합해서 중복 줄여줄 수 있을 것 같음
-    @IBAction func firstButtonPressed(_ sender: UIButton) {
-        principleModel.nextView() // 두 번째 뷰로 넘어가기 위해 카운트 + 1
-        updateUI()
-    }
-    
-    // 이전 버튼 눌렀을 때 뷰 업데이트 해주는 함수
-    @IBAction func prevButtonPressed(_ sender: UIButton) {
-        principleModel.prevView() // 이전 뷰로 돌아가기 위해 카운트 - 1
-        updateUI()
-    }
-    
-    // 다음 버튼 눌렀을 때 뷰 업데이트 해주는 함수
-    @IBAction func nextButtonPressed(_ sender: UIButton) {
-        principleModel.nextView() // 다음 뷰로 넘어가기 위해 카운트 + 1
-        updateUI()
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender == prevButton {
+            // prev버튼들
+            principleModel.prevView() // 이전 뷰로 돌아가기 위해 카운트 - 1
+            updateUI()
+        } else if principleModel.pageCount == principleModel.contents.count - 1 {
+            // 마지막 페이지 즉 pageCount가 13, progressNum이 14인 경우에만 해당
+            self.performSegue(withIdentifier: "goToEnd", sender: self)
+            // 마지막 next버튼 누르면 완료 페이지로 넘어가게 하기 위한 코드
+        } else {
+            // 마지막 페이지 이전의 모든 next버튼들(첫 번째 페이지 포함)
+            principleModel.nextView() // 다음 뷰로 넘어가기 위해 카운트 + 1
+            updateUI()
+        }
     }
     
     // UI 업데이트 해주는 함수
@@ -51,14 +57,42 @@ class PrincipleViewController: UIViewController {
         principleSubtitle.text = principleModel.getSubtitle()
         principleImage = principleModel.getImage()
         explanation.text = principleModel.getInfo()
+        showButtonView()
         if principleModel.pageCount == 0 { // 첫 번째 뷰에서는 큰 next 버튼만 보이게 하고 작은 prev, next버튼은 숨김
             firstButton.isHidden = false
             prevButton.isHidden = true
             nextButton.isHidden = true
-        } else { // 이외의 뷰에서는 큰 next 버튼 숨기고 작은 prev, next버튼은 보이게
-            firstButton.isHidden = true
-            nextButton.isHidden = false
-            prevButton.isHidden = false
+        }
+    }
+    
+    func showButtonView() {
+        firstButton.isHidden = true
+        nextButton.isHidden = false
+        prevButton.isHidden = false
+        gConsonants.isHidden = true
+        nConsonants.isHidden = true
+        mConsonants.isHidden = true
+        sConsonants.isHidden = true
+        hConsonants.isHidden = true
+        basicVowels.isHidden = true
+        compoundVowels.isHidden = true
+        hangulLabel.isHidden = true
+        if principleModel.pageCount == 5 {
+            gConsonants.isHidden = false
+        } else if principleModel.pageCount == 6 {
+            nConsonants.isHidden = false
+        } else if principleModel.pageCount == 7 {
+            mConsonants.isHidden = false
+        } else if principleModel.pageCount == 8 {
+            sConsonants.isHidden = false
+        } else if principleModel.pageCount == 9 {
+            hConsonants.isHidden = false
+        } else if principleModel.pageCount == 11 {
+            basicVowels.isHidden = false
+        } else if principleModel.pageCount == 12 {
+            compoundVowels.isHidden = false
+        } else if principleModel.pageCount == 13 {
+            hangulLabel.isHidden = false
         }
     }
 }
