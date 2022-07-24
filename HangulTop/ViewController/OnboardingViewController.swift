22decc0b9dbd0abd1ab4a883a7e14611d9c0998f
100seo <97668338+100seo@users.noreply.github.com>
2022-07-24 17:20:20 +0900

    Add: Onboarding View file
    온보딩 뷰 파일 생성

---
+++ HangulTop/ViewController/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  HangulTop
+//
+//  Created by baek seohyeon on 2022/07/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
