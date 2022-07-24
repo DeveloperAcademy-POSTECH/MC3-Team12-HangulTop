05a3c2e83f42742efb2e9a5da517953199f59531
JIMIN KIM <jim4020key@gmail.com>
2022-07-25 00:54:23 +0900

    Feat: Add navigation to QuizView (#37)

---
+++ HangulTop/ViewController/QuizViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuizViewController.swift
+//  HangulTop
+//
+//  Created by kimjimin on 2022/07/25.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
