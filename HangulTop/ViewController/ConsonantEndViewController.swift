617730c9ff75fc7b80cc578a610842116163d639
Green <76623853+jim4020key@users.noreply.github.com>
2022-07-27 13:32:58 +0900

    :twisted_rightwards_arrows: Refactor: StudyView (#47)
    * Feat: Add Images to SyllableStartView
    
    * Style: Change Variable Names
    
    * Feat: Add messageLabel to ConsonantEndView
    
    * Refactor: Delete Unused Files
    
    * Fix: Update contents when view appears (#46)

--- HangulTop/ViewController/ConsonantEndViewController.swift
+++ HangulTop/ViewController/ConsonantEndViewController.swift
@@ -13,76 +13,44 @@ struct Info {
 
 class ConsonantEndViewController: UIViewController {
     var data: Int? = 0
+    let messageArray = ["모음 학습을 마쳤습니다.", "자음 학습을 마쳤습니다.", "받침 학습을 마쳤습니다."]
     
+    @IBOutlet weak var message: UILabel!
     @IBOutlet weak var goToStudy: UIButton!
-    
-    @IBAction func goToMain(_ sender: Any) {
+    @IBAction func goToMenu(_ sender: Any) {
         //네비게이션 스택 중 원하는 지점으로 보내줌
         let controllers = self.navigationController?.viewControllers
-
-
-
         for vc in controllers! {
             if vc is StudyMenuViewController {
                         _ = self.navigationController?.popToViewController(vc as! StudyMenuViewController, animated: true)
             }
         }
-//        self.navigationController?.popToRootViewController(animated: true)
     }
     
     @IBAction func goToStudy(_ sender: Any) {
-        
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         if(appDelegate?.infos.indexCount ?? 0 > 1){
             appDelegate?.infos.indexCount = 2
         }else{
-            appDelegate?.infos.indexCount = (appDelegate?.infos.indexCount ?? 0) + 2
+            appDelegate?.infos.indexCount = (appDelegate?.infos.indexCount ?? 0) + 1
         }
-        
         let controllers = self.navigationController?.viewControllers
-
         for vc in controllers! {
             if vc is SyllableStartViewController {
                         _ = self.navigationController?.popToViewController(vc as! SyllableStartViewController, animated: true)
             }
         }
-//
-//        self.navigationController?.popViewController(animated: true)
-        
-        //네비게이션 스택 중 원하는 지점으로 보내줌
-//        let controllers = self.navigationController?.viewControllers
-//
-//
-//
-//        for vc in controllers! {
-//            if vc is ConsonantViewController {
-//                        _ = self.navigationController?.popToViewController(vc as! ConsonantViewController, animated: true)
-//            }
-//        }
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        message.text = messageArray[data ?? 0]
         if(data == 0){
             goToStudy.setTitle("자음 공부하러 가기", for: .normal)
         }else if(data == 1){
             goToStudy.setTitle("받침 공부하러 가기", for: .normal)
         }else{
-            goToStudy.setTitle("공부 끝! 클릭 X", for: .normal)
+            goToStudy.isHidden = true
         }
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
