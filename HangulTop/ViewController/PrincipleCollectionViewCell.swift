93ba5c28bd09ee9a70e95100b3bd7c6cbfe313c9
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-07-27 12:36:14 +0900

    :sparkles: Create PricipleView Sound feat
    창제원리 뷰 음성 추가

--- HangulTop/ViewController/PrincipleCollectionViewCell.swift
+++ HangulTop/ViewController/PrincipleCollectionViewCell.swift
@@ -6,7 +6,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PrincipleCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var cellButton: UIButton!
+    
+    @IBAction func speakText(_ sender: Any) {
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: (sender as AnyObject).titleLabel?.text ?? "")
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        
+        utterance.rate = 0.6
+        synthesizer.speak(utterance)
+    }
+    
 }
