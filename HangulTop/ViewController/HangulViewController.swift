764feae87fd79ce80c0ec130a36fed38d17348cd
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-07-28 16:59:09 +0900

    :sparkles: Add ContainerView

--- HangulTop/ViewController/HangulViewController.swift
+++ HangulTop/ViewController/HangulViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HangulViewController: UIViewController {
 
     @IBOutlet weak var mainLetter: UILabel!
+    @IBOutlet weak var consonantView: UIView!
+    @IBOutlet weak var vowelView: UIView!
+    @IBOutlet weak var batchimView: UIView!
     var hangul: String = "가"
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +21,67 @@ class HangulViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func viewSelect(_ sender: Any) {
+    @IBAction func viewSelect(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0) {
+            vowelView.isHidden = false
+            consonantView.isHidden = true
+            batchimView.isHidden = true
+        }else if(sender.selectedSegmentIndex == 1) {
+            vowelView.isHidden = true
+            consonantView.isHidden = false
+            batchimView.isHidden = true
+        }else if(sender.selectedSegmentIndex == 2) {
+            vowelView.isHidden = true
+            consonantView.isHidden = true
+            batchimView.isHidden = false
+        }
+    }
+    
+    @IBAction func selectButton(_ sender: UIButton){
+        let mainUni = UnicodeScalar(hangul)?.value
+        let buttonUni = UnicodeScalar(sender.titleLabel!.text ?? "\u{1100}")?.value ?? 0x1100
+        if(buttonUni < 0x1113){ //자음일때
+            let uni = buttonUni - 0x1100
+            let vowelUni = ((mainUni ?? 0xac01) - 0xac00) / 28 % 21
+            let batUni = ((mainUni ?? 0xac01) - 0xac00) % 28
+            let letter = ((uni * 21) + vowelUni) * 28 + batUni + 0xAC00
+            hangul = String(UnicodeScalar(letter)!)
+        }
+        else if(buttonUni < 0x11c3){ //받침일때
+            let uni = buttonUni - 0x11a7
+            let conUni = ((mainUni ?? 0xac01) - 0xac00) / 28 / 21
+            let vowelUni = ((mainUni ?? 0xac01) - 0xac00) / 28 % 21
+            let letter = ((conUni * 21) + vowelUni) * 28 + uni + 0xAC00
+            hangul = String(UnicodeScalar(letter)!)
+        }
+        else{ //모음일때
+            let uni = buttonUni - 0x314f
+            print("\(uni)")
+            let conUni = ((mainUni ?? 0xac01) - 0xac00) / 28 / 21
+            print("\(conUni)")
+            let batUni = ((mainUni ?? 0xac01) - 0xac00) % 28
+            print("\(batUni)")
+            let letter = ((conUni * 21) + uni) * 28 + batUni + 0xAC00
+            print("\(letter)")
+            hangul = String(UnicodeScalar(letter)!)
+            print("\(hangul)")
+        }
+        mainLetter?.text = hangul
+        print("\(hangul)")
+        pronounce(hangul)
+    }
+    func pronounce(_ letter: String) {
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: letter)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        
+        let audioSession = AVAudioSession.sharedInstance() //녹음 및 재생이 점유하고 있는 오디오 다시 가져오기
+        try! audioSession.setCategory(
+            AVAudioSession.Category.playback,
+            options: AVAudioSession.CategoryOptions.mixWithOthers
+        )
+        utterance.rate = 0.4
+        synthesizer.speak(utterance)
     }
     
     /*
