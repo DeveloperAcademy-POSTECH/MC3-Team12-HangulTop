28ba968534b3fc3ba062005dbdefee5d7b0a17cf
Juhwa Lee <juhwa.lee1023@gmail.com>
2022-08-01 02:53:42 +0900

    :twisted_rightwards_arrows: :heavy_plus_sign: Add Lottie-ios (#74)
    * :heavy_plus_sign: Add Lottie-ios
    
    로티 ios 라이브러리를 추가하였습니다.
    
    * :bento:Add json file
    
    json 파일을 추가하였습니다.

---
+++ HangulTop/LottieManage/LottieManage.swift
@@ -0,0 +1,14 @@
+//
+//  LottieManage.swift
+//  HangulTop
+//
+//  Created by 이주화 on 2022/08/01.
+//
+
+import UIKit
+import Lottie
+
+//https://fomaios.tistory.com/entry/Swift-%EB%A1%9C%ED%8B%B0-%EC%95%A0%EB%8B%88%EB%A9%94%EC%9D%B4%EC%85%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0Swift-Lottie-Animation
+
+//https://babbab2.tistory.com/142
+
